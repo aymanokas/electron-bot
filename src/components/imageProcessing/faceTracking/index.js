241c4cb58bc1c04ee6e837fa495202d683572fe9
aymanokas <solo-black@live.com>
2019-04-16 18:21:35 +0100

    remove comments

--- src/components/imageProcessing/faceTracking/index.js
+++ src/components/imageProcessing/faceTracking/index.js
@@ -10,10 +10,8 @@ class FaceTracking extends Component {
      
     let video = window.document.getElementById('video')
     let canvas = document.getElementById('canvas')
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia && video) {
-      // Not adding `{ audio: true }` since we only want video now
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia && video) { 
       navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-          //video.src = window.URL.createObjectURL(stream);
           video.srcObject = stream;
           video.play();
       });
@@ -21,20 +19,15 @@ class FaceTracking extends Component {
     var objects = new tracking.ObjectTracker(["face", "eye", "mouth"]);
     objects.on('track', function(event) {
       if (event.data.length === 0) {
-        // No objects were detected in this frame.
       } else {
         event.data.forEach(function(rect) {
-          // rect.x, rect.y, rect.height, rect.width
         });
       }
     });
     tracking.track('video', objects);
-    // let width = 700
-    // let height = 500
-    // let pixels = 500
-    // let self = this
   
   }
+  
 render(){
     let {classes} = this.props
 return (
